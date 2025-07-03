99c319733ddfcd4ceb2effbdb09c144d03800d3a
mdasadur5027 <mdasadur5027@gmail.com>
2025-07-04 02:08:15 +0600

    Updated BeamDiagram.js

--- src/components/visualizations/BeamDiagram.js
+++ src/components/visualizations/BeamDiagram.js
@@ -135,7 +135,7 @@ const BeamDiagram = ({ beamData, results }) => {
         ctx.beginPath();
         ctx.moveTo(x - 15, y + 20);
         ctx.lineTo(x, y);
-        ctx.lineTo(x + 15, y + 20);
+        ctx.lineTo(x + 15, y + 10);
         ctx.lineTo(x - 15, y + 20);
         ctx.stroke();
         break;
